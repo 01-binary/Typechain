72eb0f3ee52e23b3ab2235ae1ecc21ad457b0c4e
dgujs <jinsoo8457@dongguk.edu>
2020-04-03 17:01:04 +0900

    #1

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var name = "jinsoo", age = 28, gender = "male";
+var sayHi = function (name, age, gender) {
+    console.log(name + " " + age + " " + gender);
+};
+sayHi(name, age);
---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "jinsoo",
+age = 28,
+gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`${name} ${age} ${gender}`)
+};
+
+sayHi(name,age);
+
+export {};
