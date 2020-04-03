62626d87c0373fdbf4b0ea15e0fcda829f27e6fe
dgujs <jinsoo8457@dongguk.edu>
2020-04-03 17:34:21 +0900

    #4

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-const name = "jinsoo",
-age = 28,
-gender = "male";
 
-const sayHi = (name: string, age: number, gender?: string): void => {
-    console.log(`${name} ${age} ${gender}`)
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+const js = new Human("jinsoo", 28,"male");
+
+
+const sayHi = (person: Human): string => {
+    return `${person.name} ${person.age} ${person.gender}`;
 };
 
-sayHi(name, age);
+console.log(sayHi(js));
 
 export { };
